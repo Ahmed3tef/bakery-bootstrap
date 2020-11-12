dbe533a86afd4a5bebe42e4b83d3ef524eec294b
Ahmed Atef <66911947+Ahmed3tef@users.noreply.github.com>
2020-11-12 20:52:19 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function(){
+	$(".hamburger-menu").click(function(){
+		$(".toggle").toggleClass("open");
+		$(".nav-list").toggleClass("open");
+		
+	});
+	AOS.init({
+		easing: 'ease',
+		duration: 1000
+	});
+});
